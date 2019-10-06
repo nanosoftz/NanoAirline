e257f61c76b90847187b2fcaa9fee70d009ffd7c
Collins Uba <uba.collins29@gmail.com>
2019-10-06 04:41:19 +0400

    Spring Boot NanoAirline

---
+++ NanoAirline/src/main/java/edu/sjsu/cmpe275/NanoAirline.java
@@ -0,0 +1,20 @@
+package edu.sjsu.cmpe275;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * The type Lab 2 application.
+ */
+@SpringBootApplication
+public class NanoAirline {
+
+    /**
+     * The entry point of application.
+     *
+     * @param args the input arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(NanoAirline.class, args);
+    }
+}
